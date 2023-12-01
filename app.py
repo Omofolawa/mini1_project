8ff381e8091ce73d5b6129996d2679b845cedfc8
omofolawa Alimi-Adeniran <122090537+Omofolawa@users.noreply.github.com>
2023-12-01 00:42:06 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configure MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Glycolysis@21'
+app.config['MYSQL_DB'] = 'expense_tracker'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM expenses')
+    expenses = cur.fetchall()
+    cur.close()
+    return render_template('index.html', expenses=expenses)
+
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+    if request.method == 'POST':
+        category = request.form['category']
+        amount = request.form['amount']
+        description = request.form['description']
+
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO expenses (category, amount, description) VALUES (%s, %s, %s)', (category, amount, description))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
